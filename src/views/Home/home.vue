dd1be5ae0c8bfd69e4ee4fd1a596475c33633f08
Milena Lima de Alcântara <milena@Milenas-MacBook-Pro.local>
2024-07-18 15:57:18 -0700

    add env config

---
+++ src/views/Home/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>HOME VIEW</h1>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: "Home",
+})
+</script>
